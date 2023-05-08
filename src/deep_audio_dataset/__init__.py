46a37b93a935cc252f1562d96b5c8292e1c71a62
Doug Dennis <douglasdennisjr@gmail.com>
2023-05-08 12:56:42 -0700

    add imported libraries to __all__

--- src/deep_audio_dataset/__init__.py
+++ src/deep_audio_dataset/__init__.py
@@ -1,3 +1,8 @@
 # module to support audio data manipulation in machine learning projects
 from .dataset import AudioDataset, MultilabelClassificationAudioDataset, RegressionAudioDataset
 
+__all__ = [
+    AudioDataset,
+    MultilabelClassificationAudioDataset,
+    RegressionAudioDataset,
+]
